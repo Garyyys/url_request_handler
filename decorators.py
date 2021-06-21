ef8f50779d4e87895857e3837476e02b85d8938d
ilya.gradoboyev <ilya.gradoboyev@solbeg.com>
2021-06-21 11:27:41 +0300

    Внесены правки в декораторы, подправлен код по стандарту PEP8. Проверил 2мя плинтерами. в Pycharm и сторонним. Конфликтов, рекомендаций не обнаружено.

--- decorators.py
+++ decorators.py
@@ -1,40 +1,35 @@
-""" Декораторы для функции обработчика. """
+""" Модуль декораторов"""
 import time
-
+import datetime
+from functools import wraps
 
 
 def logging_data(func):
-
     """ Декоратор записывает входные и выходные данные функции. """
-
     input_data = []
     output_data = []
 
+    @wraps(func)
     def wrapper(*args):
-        nonlocal input_data
-        nonlocal output_data
-
-        input_data.append(*args)
         result = func(*args)
+        input_data.append(*args)
         output_data.append(result)
-
         print(f'Входные данные функции - {func.__name__}: {input_data}')
         print(f'Везультат функции - {func.__name__}: {output_data}\n')
-
     return wrapper
 
+
 def test_time(func):
-
     """ Отмечает затраченное время выполнения функции. """
-
+    @wraps(func)
     def wrapper(*args):
-
+        current_date_time = datetime.datetime.now()
+        current_time = current_date_time.time()
+        print(f'Время начала: {current_time}')
         start_time = time.time()
         result = func(*args)
         end_time = time.time() - start_time
-
         print(f'Время работы функции {end_time} сек\n')
-
+        print(f'Текущее время: {current_time}')
         return result
-
     return wrapper
--- handler.py
+++ handler.py
@@ -3,14 +3,12 @@ import requests
 import decorators
 
 
-
 @decorators.test_time
 @decorators.logging_data
 def make_request(url):
-    """ Функция обработки get запроса"""
+    """ Функция обработки get запроса."""
     response = requests.get(f'https://{url}')
     return response.text
-
 
 
 if __name__ == '__main__':
